50a8db97a0e2df731fb937511fefb30902337605
sureshmahar <sureshmahar11@gmail.com>
2021-01-21 18:01:12 +0530

    added code for stach practice

--- java/main/src/Hello3.java
+++ java/main/src/Hello3.java
@@ -1,5 +1,5 @@
 public class Hello3{
  public static void main(String[] arge){
-     System.out.prinltn("Hello3 world update from local");
+     System.out.prinltn("Hello3 world update from local and updaed and push to github");
    }
 }

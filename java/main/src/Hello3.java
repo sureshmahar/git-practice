f952df896e0f597262c7865b200a2a6f4ae8f864
sureshmahar <sureshmahar11@gmail.com>
2021-01-21 18:03:58 +0530

    stash practice

--- java/main/src/Hello3.java
+++ java/main/src/Hello3.java
@@ -1,5 +1,9 @@
 public class Hello3{
  public static void main(String[] arge){
+<<<<<<< Updated upstream
      System.out.prinltn("Hello3 world update from local and updaed and push to github");
+=======
+     System.out.prinltn("Hello3 world updated in git-practice folder");
+>>>>>>> Stashed changes
    }
 }
